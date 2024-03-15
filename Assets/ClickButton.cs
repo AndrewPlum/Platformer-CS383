088901939c6d4d796e55b7558a73e3e401eab1b3
Rebecca Smith <musebecca@gmail.com>
2024-03-15 14:36:07 -0700

    Add MainMenu and buttons

---
+++ Assets/ClickButton.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using TMPro;
+using UnityEditor.SceneManagement;
+using UnityEngine.SceneManagement;
+
+public class ClickButton : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    [SerializeField] private Image img;
+    [SerializeField] private Sprite up, pressed;
+    // [SerializeField] private AudioClip compressClip, uncompressClip;
+    // [SerializeField] private AudioSource audioSource;
+    [SerializeField] private TextMeshProUGUI playText;
+    [SerializeField] private Vector3 moveOffset;
+    
+    public void OnPointerDown(PointerEventData eventData) {
+        img.sprite = pressed;
+        // audioSource.PlayOneShot(compressClip);
+        if(playText != null){
+            playText.rectTransform.localPosition += moveOffset;
+        }
+    }
+    public void OnPointerUp(PointerEventData eventData){
+        img.sprite=up;
+        // audioSource.PlayOneShot(uncompressClip);
+        if(playText != null){
+            playText.rectTransform.localPosition -= moveOffset;
+        }
+    }
+
+    public void SwitchScene(string sceneName){
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void LeaveTheGame(){
+        #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+        #else
+        Application.Quit();
+        #endif
+    }
+}
